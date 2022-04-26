4c6ccb1a08af5c732f09bed60632984be2126ece
Suvel Rathneswar <find.suvel@gmail.com>
2022-04-26 20:14:44 +0530

    updated contact email id

---
+++ build/precache-manifest.78be911a159e9dce09103d34754b1161.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a45c9ec91d675050c3392bf7846384c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "243a225bf247f801938c",
+    "url": "/static/css/main.221dcf58.chunk.css"
+  },
+  {
+    "revision": "c3d8cce09ba533233ed9",
+    "url": "/static/js/2.84625262.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.84625262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243a225bf247f801938c",
+    "url": "/static/js/main.62632706.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  },
+  {
+    "revision": "2312ec57b28b87679088d5349fc21b4c",
+    "url": "/static/media/DMSerifDisplay-Regular.2312ec57.ttf"
+  },
+  {
+    "revision": "513397cbdf62277ff5d3a0f48fe743da",
+    "url": "/static/media/Ellipse-darktheme.513397cb.svg"
+  },
+  {
+    "revision": "532c6eed8779e6a99ce15ef494bde084",
+    "url": "/static/media/Ellipse.532c6eed.svg"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "bd19ad60600a1537c00d3b4923a5e5de",
+    "url": "/static/media/Roboto-MediumItalic.bd19ad60.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "52f9b35f9f7cfa1be2644bcbac61a983",
+    "url": "/static/media/RobotoCondensed-Bold.52f9b35f.ttf"
+  },
+  {
+    "revision": "0e1821fdf320fddc0e1c2b272c422068",
+    "url": "/static/media/RobotoCondensed-Regular.0e1821fd.ttf"
+  },
+  {
+    "revision": "61eb6510d602e4ed9e93d26019755b6c",
+    "url": "/static/media/bgdull-img.61eb6510.svg"
+  },
+  {
+    "revision": "8c3729bb2d5641dc0b9da779e8c11972",
+    "url": "/static/media/billgates-img.8c3729bb.png"
+  },
+  {
+    "revision": "5912472acf5834924be2fe9fdb53b495",
+    "url": "/static/media/guywithmic2-darktheme.5912472a.png"
+  },
+  {
+    "revision": "87fc57e6eb21dfd848695a0bf60b31e1",
+    "url": "/static/media/guywithmic2.87fc57e6.png"
+  },
+  {
+    "revision": "da9647be0c25e4e57b5b5414baef98ab",
+    "url": "/static/media/mark-img.da9647be.png"
+  },
+  {
+    "revision": "373edf061fecd746def7f8aca4f8d8de",
+    "url": "/static/media/menubar-darktheme.373edf06.svg"
+  },
+  {
+    "revision": "73c838f46bd1a5e35feec97d5707659a",
+    "url": "/static/media/menubar.73c838f4.svg"
+  }
+]);
